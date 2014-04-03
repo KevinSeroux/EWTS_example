96503360a7906897c4e96fb95f1b4eeb87642bee
Kévin Seroux <kevin.seroux@orange.fr>
2014-04-03 10:22:55 +0200

    The id and password field are now required

--- index.php
+++ index.php
@@ -52,11 +52,11 @@
 		<form role="form" method="post" action="traitement.php">
   			<div class="form-group">
     				<label>Email address</label>
-    				<input type="text" class="form-control" name="username" placeholder="Entrez votre identifiant">
- 			</div>
+    				<input type="text" class="form-control" name="username" placeholder="Entrez votre identifiant" required />
+ 			</div
   			<div class="form-group">
     				<label>Password</label>
-    				<input type="password" class="form-control" name="password" placeholder="Mot de passe">
+    				<input type="password" class="form-control" name="password" placeholder="Mot de passe" required />
   			</div>
   			<button type="submit" class="btn btn-default">Envoyer</button>
 		</form>
